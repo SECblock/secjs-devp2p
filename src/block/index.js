817f44d1e2376e3fc237211d445f5ca5a805e0b5
abysmli <abysmli@gmail.com>
2018-06-15 13:17:16 +0200

    for publish

--- src/block/index.js
+++
@@ -1,7 +0,0 @@
-let TokenBlock = require('./utils/secjs-token-blockchain')
-let TransactionBlock = require('./utils/secjs-transaction-block')
-
-exports.module = {
-    TokenBlock: TokenBlock,
-    TransactionBlock: TransactionBlock
-} 
